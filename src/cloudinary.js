f2eb29ab9a0df7a25e41519f9a6f2cf245cf6fa2
Oscar <oscalberto03@gmail.com>
2025-11-17 22:10:21 -0500

    Actualizacion1

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -1,13 +1,14 @@
-// cloudinary.js
+// src/cloudinary.js
 import { v2 as cloudinary } from 'cloudinary';
 import { config } from 'dotenv';
 config();
 
+// Opción buena: usar variables de entorno
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true
+  secure: true,
 });
 
 export default cloudinary;
